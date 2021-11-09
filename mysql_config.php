4e7f636f62f60306a34bea59fa3019ef0065362e
nahimr <nahim.rouidjali@gmail.com>
2021-11-09 12:22:53 +0100

    Add settings with docker

---
+++ mysql_config.php
@@ -0,0 +1,7 @@
+<?php
+
+$cfg['Servers'][$i]['auth_type'] = 'config';
+$cfg['Servers'][$i]['host'] = 'web-phpmyadmin';
+$cfg['Servers'][$i]['user'] = 'root';
+$cfg['Servers'][$i]['password'] = 'free_me_babe_after_malloc';
+    
