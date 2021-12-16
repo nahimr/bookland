5dd02039783c1f4ff6cb78053985a86090c79fe1
nahimr <nahim.rouidjali@gmail.com>
2021-12-16 21:55:22 +0100

    Make  nullable

--- code/src/Repository/LivreRepository.php
+++ code/src/Repository/LivreRepository.php
@@ -31,7 +31,7 @@ class LivreRepository extends ServiceEntityRepository
      * @return Livre[]
      */
 
-    public function filter(String $titre, ?DateTime $fromDate, ?DateTime $toDate,
+    public function filter(?String $titre, ?DateTime $fromDate, ?DateTime $toDate,
                            ?int $fromScore, ?int $toScore,
                            bool $respectParity, bool $distinctNationality)
     {
